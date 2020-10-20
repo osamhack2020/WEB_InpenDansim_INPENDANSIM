18137c7f379f07154eadb63bcf21f44b824e42a5
woosukji <woosukji@snu.ac.kr>
2020-10-20 14:53:43 +0000

    ADD: Adding writing page menu bar logic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <router-link to="/write/mail">인편쓰러가기</router-link>
     <router-link to="/write/rolling">롤링페이퍼 쓰러가기</router-link>
+    <div></div>
+    <textarea :value="text" @input="text = $event.target.value"></textarea>
+    <div> -> {{ text.replace(/\n/ig, '&lt;br&gt;').replace(/'/gi,'‘') }}</div>
   </div>
 </template>
 
@@ -10,7 +13,8 @@ export default {
   data() {
     return {
       searchKeyword: "",
-      news: "test"
+      news: "test",
+      text: "'News'\nParser"
     };
   }
 };
