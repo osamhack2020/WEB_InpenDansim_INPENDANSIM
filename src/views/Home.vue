6c62c254deeccfe86e2a35178159d9d8161bd929
woosukji <woosukji@snu.ac.kr>
2020-10-21 12:17:08 +0000

    ADD: Adding contents to news & music tab in Writing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <router-link to="/write/rolling">롤링페이퍼 쓰러가기</router-link>
     <div></div>
     <textarea :value="text" @input="text = $event.target.value"></textarea>
-    <div> -> {{ text.replace(/\n/ig, '&lt;br&gt;').replace(/'/gi,'‘') }}</div>
+    <div>{{ text.replace(/\n/ig, '&lt;br&gt;').replace(/'/gi,'‘') }}</div>
   </div>
 </template>
 
