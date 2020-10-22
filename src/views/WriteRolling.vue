dd07308cccce81d47446275b64e2cfb1fa1cc102
WebPr0grammer <dbwjdgh0506@gmail.com>
2020-10-22 14:10:15 +0000

    for updating

--- src/views/WriteRolling.vue
+++ src/views/WriteRolling.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="rolling">
     <div v-if="pageNumber">
+      <div class="wrap__container">
       <div>내가 찾는 훈련병은 <input />에요.</div>
       <div>우리만의 암구호는 <input />에요.</div>
       <button @click="toggleNext">새 롤링페이퍼 만들기</button>
+      </div>
     </div>
 
     <div v-else>
@@ -27,3 +29,9 @@ export default {
   }
 };
 </script>
+
+<style scoped lang="stylus">
+  .wrap__container{
+    border:1px solid #ffffff;
+  }
+</style>
